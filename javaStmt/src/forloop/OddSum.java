43ab52bbf8a4813952d7b00655db8de9ea263435
lsy <lhh1028@gmail.com>
2016-03-10 11:13:01 +0900

    java statment 프로젝트 시작

---
+++ javaStmt/src/forloop/OddSum.java
@@ -0,0 +1,32 @@
+package forloop;
+
+/**
+ * @file:OddSum.java
+ * @author lsy
+ * @date 2016. 3. 10.
+ * @story 1부터 10까지 홀수의 합
+ */
+public class OddSum {
+	public static void main(String[] args) {
+
+		int sum = 0, i=0;
+		System.out.println("1번째방법");
+		for (i = 1; i < 11; i++) {
+			if (i % 2 == 1) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		
+		System.out.println();
+		
+		sum=0;
+		System.out.println("2번째방법");
+		for(i=1; i<=10; i+=2){
+			
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+
+}
